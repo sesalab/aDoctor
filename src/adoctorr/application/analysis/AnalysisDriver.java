68b8fe811f0cb7f57966e9a35df1d81db49025cd
emaiannone <emaiannone96@gmail.com>
2019-01-26 17:06:06 +0100

    Change Request #6 applied: AnalysisHelper was created in order to increase the cohesion of AnalysisDriver.

--- src/adoctorr/application/analysis/AnalysisDriver.java
+++ src/adoctorr/application/analysis/AnalysisDriver.java
@@ -10,10 +10,6 @@ import beans.PackageBean;
 import com.intellij.openapi.project.Project;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
-import org.eclipse.jdt.core.dom.TypeDeclaration;
-import parser.CodeParser;
-import process.FileUtilities;
-import process.FolderToJavaProjectConverter;
 
 import java.io.File;
 import java.io.IOException;
@@ -24,10 +20,12 @@ import java.util.HashMap;
 public class AnalysisDriver {
 
     private Project project;
+    private AnalysisHelper analysisHelper;
     private AnalysisThread analysisThread;
 
     public AnalysisDriver(Project project) {
         this.project = project;
+        this.analysisHelper = new AnalysisHelper();
     }
 
     public Project getProject() {
@@ -39,7 +37,7 @@ public class AnalysisDriver {
     }
 
     public ArrayList<MethodSmell> startAnalysis() throws InterruptedException {
-        analysisThread = new AnalysisThread(project, this);
+        this.analysisThread = new AnalysisThread(this);
         analysisThread.start();
         synchronized (analysisThread) {
             analysisThread.wait();
@@ -52,13 +50,11 @@ public class AnalysisDriver {
     }
 
     private static class AnalysisThread extends Thread {
-        private Project project;
         private AnalysisDriver analysisDriver;
         private volatile ArrayList<MethodSmell> result;
         private volatile boolean stop;
 
-        private AnalysisThread(Project project, AnalysisDriver analysisDriver) {
-            this.project = project;
+        private AnalysisThread(AnalysisDriver analysisDriver) {
             this.analysisDriver = analysisDriver;
             result = null;
             stop = false;
@@ -79,7 +75,7 @@ public class AnalysisDriver {
                     return null;
                 }
                 // Very very slow!
-                ArrayList<PackageBean> projectPackageList = analysisDriver.buildPackageList(project);
+                ArrayList<PackageBean> projectPackageList = analysisDriver.buildPackageList();
                 if (projectPackageList == null) {
                     return null;
                 }
@@ -87,7 +83,7 @@ public class AnalysisDriver {
                 if (stop) {
                     return null;
                 }
-                ArrayList<File> javaFilesList = analysisDriver.getAllJavaFiles(project);
+                ArrayList<File> javaFilesList = analysisDriver.getAllJavaFiles();
                 if (javaFilesList == null) {
                     return null;
                 }
@@ -106,97 +102,19 @@ public class AnalysisDriver {
                     }
                     try {
                         return analysisDriver.analyze(projectPackageList, sourceFileMap);
-                    } catch (IOException e3) {
-                        e3.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
                         return null;
                     }
-                } catch (IOException e2) {
-                    e2.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
                     return null;
                 }
-            } catch (IOException e1) {
-                e1.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
                 return null;
             }
         }
-    }
-
-    /**
-     * Builds an ArrayList of PackageBean, a legacy structure from aDoctor.
-     * This will be used during the building of the list of code smells.
-     *
-     * @param project
-     * @return
-     */
-    private ArrayList<PackageBean> buildPackageList(Project project) throws IOException {
-        ArrayList<PackageBean> packageList = null;
-        if (project != null) {
-            String projectBasePath = project.getBasePath();
-            if (projectBasePath != null) {
-                File projectDirectory = new File(project.getBasePath());
-
-                // Calling the aDoctor legacy method to build this list
-                packageList = FolderToJavaProjectConverter.convert(projectDirectory.getAbsolutePath());
-
-                // belongingClass was not set in aDoctor API: this is just a fix
-                for (PackageBean packageBean : packageList) {
-                    for (ClassBean classBean : packageBean.getClasses()) {
-                        for (MethodBean methodBean : classBean.getMethods()) {
-                            methodBean.setBelongingClass(classBean);
-                        }
-                    }
-                }
-            }
-        }
-        return packageList;
-    }
-
-    /**
-     * Obtains all project Java files
-     *
-     * @param project
-     * @return
-     */
-    private ArrayList<File> getAllJavaFiles(Project project) {
-        ArrayList<File> javaFilesList = null;
-        if (project != null) {
-            String projectBasePath = project.getBasePath();
-            if (projectBasePath != null) {
-                File projectDirectory = new File(project.getBasePath());
-
-                // Invokes the recursive function to get all .java files
-                javaFilesList = getJavaFilesInDirectory(projectDirectory);
-            }
-        }
-        return javaFilesList;
-    }
-
-    /**
-     * Builds a HashMap that given a class FQN it is possible to get the related java File.
-     *
-     * @param javaFilesList
-     * @return
-     */
-    private HashMap<String, File> buildSourceFileMap(ArrayList<File> javaFilesList) throws IOException {
-        HashMap<String, File> sourceFileMap = null;
-        if (javaFilesList != null && javaFilesList.size() > 0) {
-            sourceFileMap = new HashMap<>();
-            for (File javaFile : javaFilesList) {
-                // Creates the CompilationUnit of every Java file in order to get its FQN easily. This is done through CodeParser of aDoctor
-                CodeParser codeParser = new CodeParser();
-                String javaFileContent = FileUtilities.readFile(javaFile.getAbsolutePath());
-                CompilationUnit compilationUnit = codeParser.createParser(javaFileContent);
-
-                // Builds the FQN String
-                String packageName = compilationUnit.getPackage().getName().getFullyQualifiedName();
-                TypeDeclaration typeDeclaration = (TypeDeclaration) compilationUnit.types().get(0);
-                String className = typeDeclaration.getName().toString();
-                String classFullName = packageName + "." + className;
-
-                sourceFileMap.put(classFullName, javaFile);
-            }
-        }
-        return sourceFileMap;
     }
 
     /**
@@ -245,31 +163,15 @@ public class AnalysisDriver {
         return smellMethodList;
     }
 
-    /**
-     * Get all Java files in the directory and then recusively does the same in all subdirectories
-     *
-     * @param directory
-     * @return
-     */
-    private ArrayList<File> getJavaFilesInDirectory(File directory) {
-        ArrayList<File> javaFilesList = new ArrayList<>();
-        // File list of the current directory
-        File[] fList = directory.listFiles();
-
-        // If the directory is non empty
-        if (fList != null) {
-            for (File file : fList) {
-                if (file.isFile()) {
-                    if (file.getName().contains(".java")) {
-                        javaFilesList.add(file);
-                    }
-                } else if (file.isDirectory()) {
-                    File subDirectory = new File(file.getAbsolutePath());
-                    javaFilesList.addAll(getJavaFilesInDirectory(subDirectory));
-                }
-            }
-        }
-        return javaFilesList;
+    private ArrayList<PackageBean> buildPackageList() throws IOException {
+        return analysisHelper.buildPackageList(project);
     }
 
+    private ArrayList<File> getAllJavaFiles() {
+        return analysisHelper.getAllJavaFiles(project);
+    }
+
+    private HashMap<String, File> buildSourceFileMap(ArrayList<File> javaFilesList) throws IOException {
+        return analysisHelper.buildSourceFileMap(javaFilesList);
+    }
 }
